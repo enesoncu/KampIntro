7bf9e26fe9d0663c677a35886b1396b0157f37ba
Enes Öncü <enesoncuu@hotmail.com>
2021-01-21 17:39:08 +0300

    20 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do not repeat yourself 
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmısMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmısMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
